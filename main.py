7b9629316ce0fbdb139f14251cceee97a03d025c
Flo <florian.rost98@web.de>
2026-01-16 15:43:53 +0100

    initial structure

---
+++ main.py
@@ -0,0 +1,36 @@
+"""
+game/
+  main.py
+  config.py
+
+  net/
+    messages.py        # Message-Format (JSON), encode/decode
+    udp_discovery.py   # Broadcast DISCOVER + OFFER Listener
+    transport.py       # Send/recv, retries/acks (optional)
+    tcp_join.py        # (optional) Join per TCP statt UDP
+
+  core/
+    state.py           # Zustandsmodell: Lobby / Round / Players
+    node.py            # GameNode: verbindet alles (Netz + Core)
+    voting.py          # Propose/YesNo/Commit
+
+  util/
+    ids.py             # UUID helpers
+    clock.py           # timeouts/backoff
+    log.py
+"""
+
+import socket
+from net import udp_discover_listener, udp_discover_sender 
+
+if __name__ == '__main__':
+ # Broadcast address and port (example for a /24 network)
+ BROADCAST_IP = "255.255.255.255"
+ BROADCAST_PORT = 5973
+ # Local host information
+ my_host = socket.gethostname()
+ my_ip = socket.gethostbyname(my_host)
+ # Compose broadcast announcement
+ announcement = my_ip + " sent a broadcast message"
+ # Send the broadcast announcement
+ udp_discover_sender.broadcast(BROADCAST_IP, BROADCAST_PORT, announcement)

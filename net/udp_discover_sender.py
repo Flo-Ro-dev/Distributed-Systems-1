1086caf7a372e2bd26c1527245e0195a60d7e65e
Silas Müller <news@silas2000.de>
2026-01-26 18:16:08 +0100

    merge

--- net/udp_discover_sender.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-def broadcast(ip, port, broadcast_announcement):
- # Create a UDP socket
- broadcast_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
- # Enable permission to send to broadcast addresses
- broadcast_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
- # Send the broadcast announcement
- broadcast_socket.sendto(str.encode(broadcast_announcement), (ip, port))
